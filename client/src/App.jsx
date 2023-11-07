5f5c270c9f609226ad7c59afd780742c630ba95a
HISHAN03 <moidinhishan@gmail.com>
2023-11-07 19:05:20 +0530

    second update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,51 @@
 import React, { useEffect, useRef,useState  } from "react";
-import socket from "./socket";
-function App() {
+import { io } from "socket.io-client";
 
+const socket = io("http://localhost:3000");
+function App() {
+  const [mySocketId, setMySocketId] = useState("");
   let localStream;
   let remoteStream;
-  
+  const [userList, setUserList] = useState([]);
     const user1VideoRef = useRef(null);
     const user2VideoRef = useRef(null);
 
-  const servers = {
-    iceServers: [
-      {
-        urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"],
-      },
-    ],
+    const servers = {
+      iceServers: [
+        {
+          urls: ["stun:stun.arbuz.ru:3478", "stun:stun.bahnhof.net:3478"],
+        },
+      ],
+    };
+    
+  let peerconnection = new RTCPeerConnection(servers);
+  
+  const send = (selectedUserId) => {
+    console.log(`Sending message to user with ID: ${selectedUserId}`);
+   const createOffer = async (targetUserId) => {
+    await CreatePeerConnection();
+    let offer = await peerconnection.createOffer();
+    await peerconnection.setLocalDescription(offer);
+    console.log("offer set as local description")
+    console.log("Offer: " + offer);
+    const offerJson = {
+      type: "offer",
+      offer: offer,
+      targetUserId: targetUserId, 
+      yourSocketId: socket.id, 
+    };
+  
+    console.log(targetUserId);
+    socket.emit("call", JSON.stringify(offerJson)); // Send the modified offerJson
+    console.log("Offer sent");
   };
-
-
-
+  
+    
+    createOffer(selectedUserId); // Pass the selectedUserId to the createOffer function.
+  };
   
 
 
-  let peerconnection = new RTCPeerConnection(servers);
 
   const CreatePeerConnection = async () => {
     remoteStream = new MediaStream();
@@ -47,15 +71,29 @@ function App() {
       }
     };
   };
+
+
+
+    socket.on("userList", (users) => {
+      // Update the userList state when the list of connected users changes
+      setUserList(users);
+      setMySocketId(socket.id);
+      console.log("user");
+    });
+
+
+
+let roomNum=123
+
   useEffect(() => {
+    socket.emit('join', roomNum);
     const init = async () => {
       localStream = await navigator.mediaDevices.getUserMedia({
         video: true,
         audio: true,
       });
       user1VideoRef.current.srcObject = localStream;
-      console.log("Working");
-      createOffer();
+      //createOffer();
     };
 
     const createOffer = async () => {
@@ -64,28 +102,47 @@ function App() {
       await peerconnection.setLocalDescription(offer);
       console.log("Offer: " + offer);
       const offerJson = JSON.stringify({ type: "offer", offer: offer });
-      socket.emit("call", offerJson);
+     socket.emit("call", offerJson);
       console.log("offer sent");
     };
+
+
+    
 
     init();
   }, []);
 
   socket.on("offer", async (offerJson) => {
+    console.log("offer")
     //await CreatePeerConnection();
     //const offer = JSON.parse(offerJson);
     await peerconnection.setRemoteDescription(offerJson.offer);
-    console.log("answer remote description set");
+    console.log("offer remote description set");
     const answer = await peerconnection.createAnswer();
     await peerconnection.setLocalDescription(answer);
-    const answerJson = JSON.stringify({ type: "answer", answer: answer });
+    console.log("answer local description set");
+
+    console.log(offerJson.targetUserId+"  target user id")
+    console.log(offerJson.yourSocketId+"  my user id")
+
+    const answerJson = JSON.stringify({ type: "answer", answer: answer, targetUserId: offerJson.targetUserId, yourSocketId:offerJson.yourSocketId });
+    
     socket.emit("answer2", answerJson);
     console.log("Answer sent");
   });
 
+
+  socket.on('message',(data)=>{
+    console.log(data+"hello this is hishan")
+  }
+  )
+
+
+
+
+
   socket.on("answer2", async (answerJson) => {
     await peerconnection.setRemoteDescription(answerJson.answer);
-
     console.log("Answer2 set");
   });
 
@@ -97,17 +154,16 @@ function App() {
         <video className="video-player" ref={user1VideoRef} autoPlay />
         <video className="video-player" ref={user2VideoRef} autoPlay />
       </div>
-      <div id="user-list">
-        <h2>Available Users:</h2>
+      <div>
+        <h2>Connected Users</h2>
         <ul>
-        <div onClick={()=> selectUser(user)}>
-
-          {usersList
-            .filter((user) => user !== myUserId) // Exclude your own username
-            .map((user, index) => (
-              <li key={index}>{user}</li>
-              ))}
-              </div>
+        {userList
+            .filter((userId) => userId !== mySocketId) // Step 2: Filter out your own socket.id
+            .map((userId) => (
+              <li key={userId} onClick={() => send(userId)}>
+                User ID: {userId}
+              </li>
+            ))}
         </ul>
       </div>
     </>
--- server/server.js
+++ server/server.js
@@ -7,34 +7,74 @@ const corsOptions = {
   origin: "http://localhost:5173",
   methods: ["GET", "POST"],
 };
+
 const io = socketIO(server, {
   cors: corsOptions,
 });
 
+users=[]
+
+
 io.on("connection", (socket) => {
+  socket.on('join', function(room) {
+    socket.join(room);
+    console.log("room joined")
+});
+  //socket.join(mainRoom);
+  users.push(socket.id)
+  console.log("list of users  "+ users)
+  io.emit("userList", users);
+  
   console.log(`A user connected with id ${socket.id}`);
+
 
   socket.on("call", (offerJson) => {
     const offer = JSON.parse(offerJson);
     if (offer.type === "offer") {
-      socket.broadcast.emit("offer", offer);
-      console.log("Offer sent to all users except the sender");
+      // Extract the target user's socket.id (e.g., from the offer data)
+    //  const targetSocketId = offer.targetUserId; 
+      const targetSocketId = offer.targetUserId;
+      const yourSocketId = offer.yourSocketId;
+      console.log("myid : "+yourSocketId)
+      console.log("sender id : "+targetSocketId)
+
+// Replace with the actual source of the targetSocketId
+      socket.to(targetSocketId).emit("offer", offer);
+      
+       if (targetSocketId) {
+    
+       console.log(`Offer sent to user with socket.id: ${targetSocketId}`);
+      } else {
+         console.log("Target user's socket.id not found in the offer.");
+      }
+     //}
     }
   });
-
+  
 
   socket.on("answer2", (answerJson) => {
+    const answerJson1 = JSON.parse(answerJson);
+    const targetSocketId = answerJson1.targetUserId;
+    const yourSocketId = answerJson1.yourSocketId;
+    console.log("my id  :"+yourSocketId)
     // Parse the received JSON string back into an object
-    const answer = JSON.parse(answerJson);
-    if (answer.type === "answer") {
-      socket.broadcast.emit("answer2", answer);
+   // const answer = JSON.parse(answerJson);
+    // if (answer.type === "answer") {
+      socket.to(yourSocketId).emit("answer2", answerJson1);
       console.log("answer sent to all users except the sender");
-    }
+    // }
     // console.log('answer is not of type answer')
   });
 
   socket.on("disconnect", () => {
     console.log(`User disconnected with id ${socket.id}`);
+
+    const index = users.indexOf(socket.id);
+    if (index !== -1) {
+      users.splice(index, 1);
+    }
+    // Emit the updated list of connected users to all clients
+    io.emit("userList", users);
     
   });
 });
